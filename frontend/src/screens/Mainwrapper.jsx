879685c39caa443ebaca7344ec5a4f0c80b04679
keval donga <kevaldonga38@gmail.com>
2024-06-29 15:57:41 +0530

    fixed header

--- frontend/src/screens/Mainwrapper.jsx
+++ frontend/src/screens/Mainwrapper.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import LoadingBar from "react-top-loading-bar";
+import LoadingBar from 'react-top-loading-bar';
 import Alertdialog from "../global/components/alertdialog";
 import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
@@ -7,22 +7,26 @@ import Header from "../global/components/header";
 import Footer from "../global/components/footer";
 
 const MainWrapper = () => {
-  const progress = useSelector((state) => state.topLoadingBarReducer.progress);
+    const progress = useSelector(state => state.topLoadingBarReducer.progress);
 
-  return (
-    <div className="fixed w-[100dvw] h-[100dvh]">
-      <Alertdialog />
+    return <div className="w-[100dvw] h-[100dvh] relative">
+        <div className="fixed w-full h-full pointer-events-none">
+            <Alertdialog />
 
-      <ToastContainer />
-      <LoadingBar height={3} color="#1d4ed8" progress={progress} />
+            <ToastContainer />
+            <LoadingBar
+                height={3}
+                color='#1d4ed8'
+                progress={progress}
+            />
+        </div>
 
-      <div className="w-full h-full flex flex-col overflow-y-auto">
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
-    </div>
-  );
+        <div className="w-full flex flex-col overflow-y-auto top-0">
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    </div>;
 };
 
 export default MainWrapper;
