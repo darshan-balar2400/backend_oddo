4279b7c979cda9859cc1dcda619dbaa446cfde44
keval donga <kevaldonga38@gmail.com>
2024-06-29 09:28:43 +0530

    added auth wrapper

--- frontend/src/RouterFile.jsx
+++ frontend/src/RouterFile.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./screens/Login";
+import AuthWrapper from "./screens/Authwrapper";
 
 const RouterFile = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<AuthWrapper />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
